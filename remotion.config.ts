c077450aeeaf25392a0b9267443f61678cea45ae
Antoine <comon.aveyron@gmail.com>
2026-05-15 16:15:52 +0200

    refactor: passe le format d'image vidéo de jpeg à png pour une meilleure qualité visuelle
    - Remplace `setVideoImageFormat("jpeg")` par `setVideoImageFormat("png")` dans `remotion.config.ts`

--- remotion.config.ts
+++ remotion.config.ts
@@ -2,8 +2,7 @@ import { Config } from "@remotion/cli/config";
 
 Config.setCodec("prores");
 Config.setProResProfile("standard");
-Config.setVideoImageFormat("jpeg");
-Config.setJpegQuality(95);
+Config.setVideoImageFormat("png");
 Config.setPixelFormat("yuv422p10le");
 Config.setColorSpace("bt709");
 Config.setOverwriteOutput(true);
